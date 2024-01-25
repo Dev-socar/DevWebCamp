89bcd4dc437070ccfa4fe49c50a0e4ba31546ebd
Dev-socar <omar.fsc19@gmail.com>
2024-01-24 23:17:00 -0600

    25 January 2024

--- controllers/DashboardController.php
+++ controllers/DashboardController.php
@@ -2,26 +2,44 @@
 
 namespace Controllers;
 
+use Model\Paquete;
+use Model\Ponente;
 use Model\Registro;
 use Model\Usuario;
 use MVC\Router;
 
-class DashboardController{
+class DashboardController
+{
 
-    public static function index(Router $router){
+    public static function index(Router $router)
+    {
 
+        $ingresos = 0;
         //obtener ultimos registros
         $registros = Registro::get(5);
-        foreach($registros as $registro){
+        foreach ($registros as $registro) {
             $registro->usuario = Usuario::find($registro->usuario_id);
+            $registro->paquete = Paquete::find($registro->paquete_id);
+            if ($registro->paquete_id === "1") {
+                $ingresos += 199;
+            } else {
+                $ingresos += 149;
+            }
         }
+        $data = [
+
+            'totalUsuarios' => Registro::total(),
+            'totalPonentes' => Ponente::total(),
+            'ingresos' => number_format($ingresos, 0, ',', ',')
+        ];
+        // debuguear($data);
+        
 
         // Render a la vista 
         $router->render('admin/dashboard/index', [
             'titulo' => 'Panel de Administración',
-            'registros' => $registros
+            'registros' => $registros,
+            'data' => $data
         ]);
     }
 }
-
-?>
--- views/admin/dashboard/index.php
+++ views/admin/dashboard/index.php
@@ -1,11 +1,61 @@
-<h2 class="dashboard__heading"><?php echo $titulo; ?></h2>
-
 <main class="bloques">
     <div class="bloques__grid">
         <div class="bloque">
+            <h3 class="bloque__heading">Total de Registrados</h3>
+            <div class="bloque__contenido">
+                <i class="fa-solid fa-users bloque__icono"></i>
+                <p class="bloque__texto"><?php echo $data['totalUsuarios'] ?></p>
+            </div>
+
+        </div>
+        <div class="bloque">
+            <h3 class="bloque__heading">Total de Ingreso</h3>
+            <div class="bloque__contenido">
+                <i class="fa-solid fa-dollar-sign bloque__icono"></i>
+                <p class="bloque__texto">$ <?php echo $data['ingresos']; ?></p>
+            </div>
+        </div>
+        <div class="bloque">
+            <h3 class="bloque__heading">Total de Ponentes</h3>
+            <div class="bloque__contenido">
+                <i class="fa-solid fa-microphone bloque__icono"></i>
+                <p class="bloque__texto"><?php echo $data['totalPonentes']; ?></p>
+            </div>
+
+        </div>
+        <div class="bloque">
             <h3 class="bloque__heading">Últimos Registros</h3>
-            <?php foreach($registros as $registro) { ?>
-                
+            <?php if (!empty($registros)) { ?>
+                <table class="table">
+                    <thead class="table__thead">
+                        <tr>
+                            <th class="table__th" scope="col">
+                                Nombre
+                            </th>
+                            <th class="table__th" scope="col">
+                                Email
+                            </th>
+                            <th class="table__th" scope="col">
+                                Paquete
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody class="table__tbody">
+                        <?php foreach ($registros as $registro) { ?>
+                            <tr class="table__tr">
+                                <td class="table__td">
+                                    <?php echo $registro->usuario->nombre . ' ' . $registro->usuario->apellido; ?>
+                                </td>
+                                <td class="table__td">
+                                    <?php echo $registro->usuario->email; ?>
+                                </td>
+                                <td class="table__td">
+                                    <?php echo $registro->paquete->nombre; ?>
+                                </td>
+                            </tr>
+                        <?php } ?>
+                    </tbody>
+                </table>
             <?php } ?>
         </div>
     </div>
